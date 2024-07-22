c7ffcd874793957f68282badccfb47725aeed55e
Egor <Jeglederefter@yandex.ru>
2024-07-23 00:54:05 +0300

    add /help command handler

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from aiogram import Bot
 from aiogram import Dispatcher
 from aiogram import types
 from aiogram.filters import CommandStart, Command
+from aiogram.utils import markdown
+from aiogram.enums import ParseMode 
 
 dp = Dispatcher()
 
@@ -15,6 +17,39 @@ async def handle_start(message: types.Message):
         - Для получения ответа от ChatGPT, введите команду /gpt \n\n- Для формирования заявки в поддержку, введите команду /support
     """
     await message.answer(text=text)
+
+
+@dp.message(Command("help"))
+async def hndle_help(message: types.Message):
+    text = markdown.text(
+        markdown.text(
+            "Меня зовут",
+            markdown.markdown_decoration.bold(
+                markdown.text(
+                    "Алекса\\,"
+                )
+            ),
+            markdown.markdown_decoration.quote(
+                "я телеграм-бот, который поможет вам решить возникшую проблему.\n\nВот список моих команд:\n\n"
+            ),
+            markdown.markdown_decoration.bold(
+                markdown.text(
+                    "📍Начало работы \- /start\n",
+                    "📍Задать вопрос ChatGPT \- /ask\_gpt\n",
+                    "📍Написать в поддержку \- /support\n"
+                )
+            )
+        ),
+        markdown.text(
+            "Для выбора, просто нажмите на нужную команду\.\n"
+        ),
+        sep="\n"
+    )
+
+    await message.answer(
+        text=text,
+        parse_mode=ParseMode.MARKDOWN_V2
+    )
 
 
 # @dp.message()
