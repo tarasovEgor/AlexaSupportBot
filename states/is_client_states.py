db43c1ff10bdc53f6775005212217d2dd4203bdc
Egor <Jeglederefter@yandex.ru>
2024-08-06 15:32:21 +0300

    states refactored

--- states/is_client_states.py
+++ states/is_client_states.py
@@ -2,3 +2,10 @@ from aiogram.fsm.state import StatesGroup, State
 
 class IsClientQuery(StatesGroup):
     full_name = State()
+    company_name = State()
+    email = State()
+    phone_number = State()
+    inn = State()
+    question = State()
+    user_is_satisfied = State()
+    has_contancted_support = State()
--- states/new_client_states.py
+++ states/new_client_states.py
@@ -2,3 +2,5 @@ from aiogram.fsm.state import StatesGroup, State
 
 class NewClientQuery(StatesGroup):
     PC_count = State()
+    server_count = State()
+    office_count = State()
