58a1deae8811171a7b4e1de5fe80655c9e132416
Egor <Jeglederefter@yandex.ru>
2024-08-07 20:36:08 +0300

    new_client_commands/states added

--- routers/commands/__init__.py
+++ routers/commands/__init__.py
@@ -6,6 +6,7 @@ from .zammad_commands import router as zammad_commands_router
 from .base_commands import router as base_commands_router
 from .gpt_commands import router as gpt_commands_router
 from .is_client_commands import router as is_client_commands_router
+from .new_client_commands import router as new_client_commands_router
 from .common import router as common_router
 
 router = Router(name=__name__)
@@ -14,4 +15,5 @@ router.include_router(zammad_commands_router)
 router.include_router(base_commands_router)
 router.include_router(gpt_commands_router)
 router.include_router(is_client_commands_router)
+router.include_router(new_client_commands_router)
 router.include_router(common_router)
--- states/new_client_states.py
+++ states/new_client_states.py
@@ -1,6 +1,16 @@
 from aiogram.fsm.state import StatesGroup, State
 
-class NewClientQuery(StatesGroup):
+class NewClientInfrastructureQuery(StatesGroup):
     PC_count = State()
     server_count = State()
     office_count = State()
+    consent_recieved = State()
+    program_type = State()
+
+
+class NewClientInfoQuery(StatesGroup):
+    city = State()
+    company_name = State()
+    company_address = State()
+    phone_number = State()
+    inn = State()
